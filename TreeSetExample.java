a241e79166e84348b83674a4da24cd71db787ffa
deepansh983 <raghudeepansh0@gmail.com>
2024-08-12 15:45:38 +0530

    Add files via upload

---
+++ TreeSetExample.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class TreeSetExample {
+    public static void main(String[] args)
+    {
+        //crearing tree set of integers
+        TreeSet<Integer> Numbers=new TreeSet<Integer>();
+
+        //insert the integer
+
+        Numbers.add(4);
+        Numbers.add(14);
+        Numbers.add(9);
+        Numbers.add(7);
+        Numbers.add(-4);
+        Numbers.add(5);
+
+        System.out.println("Numbers are :"+Numbers);
+    }
+    
+}
